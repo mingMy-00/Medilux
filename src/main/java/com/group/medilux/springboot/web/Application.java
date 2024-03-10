3070820fc6d5262b8a17d26cde2d2be49ba43993
mingMy-00 <149574203+mingMy-00@users.noreply.github.com>
2024-03-11 00:54:30 +0900

    [2024-03-11] lombok test 추가

--- src/main/java/com/group/medilux/springboot/web/Application.java
+++ src/main/java/com/group/medilux/springboot/web/Application.java
@@ -2,10 +2,10 @@ package com.group.medilux.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);}
-
 }
--- src/main/java/com/group/medilux/springboot/web/HelloController.java
+++ src/main/java/com/group/medilux/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.group.medilux.springboot.web;
 
+import com.group.medilux.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +10,11 @@ public class HelloController {
 
     @GetMapping(value = "/hello")
     public String hello() {
-        return "hello";
+        return "hellohihihi";
     }
 
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
