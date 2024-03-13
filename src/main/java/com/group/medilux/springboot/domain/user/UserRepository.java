cbb1eaa6111a4408a90a3a85f9a667491cf95981
mingMy00 <mingmy00@ssts.co.kr>
2024-03-13 17:10:34 +0900

    [2024.03.13] 구글 로그인 2 (클래스 생성)

---
+++ src/main/java/com/group/medilux/springboot/domain/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.group.medilux.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //이미 가입한 회원인지 확인해주는 method
+    @Override
+    Optional<User> findById(Long aLong);
+}
