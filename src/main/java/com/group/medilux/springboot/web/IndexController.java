f6f559e7a1f9c86a365da1e6376ca2f73e5d3ff0
mingMy00 <mingmy00@ssts.co.kr>
2024-03-13 13:44:43 +0900

    2024.03.13 [게시글 수정]

--- src/main/java/com/group/medilux/springboot/web/IndexController.java
+++ src/main/java/com/group/medilux/springboot/web/IndexController.java
@@ -1,11 +1,13 @@
 package com.group.medilux.springboot.web;
 
 import com.group.medilux.springboot.service.PostsService;
+import com.group.medilux.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,16 +15,25 @@ public class IndexController {
 
     private final PostsService postsService;
 
+    //처음 화면에 게시글 불러오기 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts" , postsService.findAllDesc());
-        System.out.println(postsService.findAllDesc());
         return "index";
     }
 
+    //게시글 등록하기
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        model.addAttribute("post" , postsService.findById(id));
+
+        return "posts-update";
     }
 
 
--- src/main/java/com/group/medilux/springboot/web/PostsApiController.java
+++ src/main/java/com/group/medilux/springboot/web/PostsApiController.java
@@ -14,16 +14,19 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    //게시글 등록
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
+    //게시글 수정
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
+    //게시글 조회
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
